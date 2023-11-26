c09ec9c6bc847ffd71e1376c758bbd31fce1a06f
Doruk Senel <seneldoruk@gmail.com>
2023-11-26 18:30:10 +0100

    KW45-46

--- solutions/js/uebungen.js
+++ solutions/js/uebungen.js
@@ -689,22 +689,413 @@ Vermeiden Sie auch hier das Erscheinen von horizontalen Scrollbars so weit wie m
 )
 kwArr.push(kw43)
 
-const navbar = document.querySelector(".horizontal")
-kwArr.forEach((kw) => {
-  const title = kw.title
 
-  const li = document.createElement('li')
-  const a = document.createElement('a')
+const kw45 = parseAsKW("KW 45",
+  [
+    {
+      q: "Eine Funktion identity(), die ein Argument als Parameter entgegennimmt und dieses als Ergebnis zurückgibt.",
+      a: `
+function identity(x) {
+  return x;
+}
+    `,
+    },
+    {
+      q: "Eine Funktion identity_function(), die ein Argument als Parameter entgegennimmt und eine Funktion zurückgibt, die dieses Argument zurückgibt.",
+      a: `
+function identity_function(x) {
+  return function() {
+    return x;
+  };
+}
+    `,
+    },
+    {
+      q: "Zwei binäre Funktionen add und mul, die Summe und Produkt berechnen.",
+      a: `
+function add(x, y) {
+  return x + y;
+}
 
-  const funcName = `${title.toLowerCase().replace(/\s/g, '')}.renderSelf(this)`;
-  a.setAttribute("onclick", funcName)
+function mul(x, y) {
+  return x * y;
+}
+    `,
+    },
+    {
+      q: "Eine Addierer-Funktion addf(), so dass addf(x)(y) genau x + y zurückgibt. (Es haben also zwei Funktionsaufrufe zu erfolgen. addf(x) liefert eine Funktion, die auf y angewandt wird.)",
+      a: `
+function addf(x) {
+  return function(y) {
+    return x + y;
+  };
+}
+    `,
+    },
+    {
+      q: "Eine Funktion applyf(), die aus einer binären Funktion wie add(x,y) eine Funktion addf berechnet, die mit zwei Aufrufen das gleiche Ergebnis liefert, z.B. addf = applyf(add); addf(x)(y) soll add(x,y) liefern.",
+      a: `
+function applyf(binaryFunc) {
+  return function(x) {
+    return function(y) {
+      return binaryFunc(x, y);
+    };
+  };
+}
+    `,
+    },
+    {
+      q: "Schreiben Sie die Prototypen Person und Auto in JavaScript, sodass jede Person weiß, welche Autos sie besitzt. Schreiben Sie eine Funktion conflict(), die feststellt, ob ein Auto von mehr als einer Person besessen wird.",
+      a: `
+function Auto(marke, modell, kennzeichen) {
+  this.marke = marke;
+  this.modell = modell;
+  this.kennzeichen = kennzeichen;
+}
 
-  const titleToRender = `- ${title} -`
-  a.textContent = titleToRender;
+function Person(name) {
+  this.name = name;
+  this.autos = [];
+}
 
-  li.appendChild(a)
-  navbar.appendChild(li)
-})
+Person.prototype.addAuto = function(auto) {
+  this.autos.push(auto);
+};
 
-navbar.children[0].firstChild.click()
+function conflict(personen) {
+  let kennzeichenMap = {};
+
+  personen.forEach(person => {
+    person.autos.forEach(auto => {
+      if (kennzeichenMap[auto.kennzeichen]) {
+        kennzeichenMap[auto.kennzeichen].push(person.name);
+      } else {
+        kennzeichenMap[auto.kennzeichen] = [person.name];
+      }
+    });
+  });
+
+  for (let kennzeichen in kennzeichenMap) {
+    if (kennzeichenMap[kennzeichen].length > 1) {
+      console.log("Conflict", kennzeichenMap[kennzeichen]);
+    }
+  }
+}
+    `,
+    },
+    {
+      q: "Schreiben Sie im Browser die Fibonacci-Funktion in JS und geben Sie die ersten 2000 Fibonacci-Zahlen 0,1,1,2,3,5,8,13,... auf der Konsole mit console.log() aus.",
+      a: `
+// Aufgabe 3
+function fibonacci(n) {
+  let fib = [0n, 1n];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log(fibonacci(2000));
+    `,
+    },
+    {
+      q: "Was ist die größte Fibonacci-Zahl, die sich noch als Integer sicher speichern lässt (Number.MAX_SAFE_INTEGER)? Die wievielte Zahl in der Fibonacci-Folge ist das?",
+      a: `
+// Aufgabe 1
+function fibonacci(n) {
+  let fib = [0n, 1n];
+  for (let i = 2; i <= n; i++) {
+    const current = fib[i - 1] + fib[i - 2];
+    if (current > Number.MAX_SAFE_INTEGER) {
+      console.log(\`\${i - 1}:\${fib[fib.length - 1]}\`);
+      return;
+    }
+    fib[i] = current;
+  }
+  return fib;
+}
+
+fibonacci(2000);
+// Output: 78: 8944394323791464
+    `,
+    },
+    {
+      q: "Was ist die größte Fibonacci-Zahl, die sich noch als Number speichern lässt (Number.MAX_VALUE)? Die wievielte Zahl in der Fibonacci-Folge ist das?",
+      a: `
+// Aufgabe 2
+function fibonacci(n) {
+  let fib = [0n, 1n];
+  for (let i = 2; i <= n; i++) {
+    const current = fib[i - 1] + fib[i - 2];
+    if (current > Number.MAX_VALUE) {
+      console.log(\`\${i - 1}:$\{fib[fib.length - 1]}\`);
+      return;
+    }
+    fib[i] = current;
+  }
+  return fib;
+}
+
+fibonacci(2000);
+// Output: 1476: 130698922376339931803631155380271983098392443907412640726006659460192793070479231740288681087777017721095463154979012276234322246936939647185366706368489362660844147449941348462800922755818969634743348982916424954062744135969865615407276492410653721774590669544801490837649161732095972658064630033793347171632
+    `,
+    },
+    {
+      q: "Wechseln Sie zu BigInt, um alle 2000 Fibonacci-Zahlen korrekt anzuzeigen.",
+      a: `
+// Aufgabe 3
+function fibonacci(n) {
+  let fib = [0n, 1n];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log(fibonacci(2000));
+    `,
+    },
+    {
+      q: "Optional: Was ist die größte Fibonacci-Zahl, die Sie mit BigInt korrekt berechnet haben? An welcher Stelle in der Fibonacci-Folge steht diese?",
+      a: "Die größte Fibonacci-Zahl, die sich mit BigInt korrekt berechnen lässt, ist 2000: 39406014605753609255676250506383186276236444006662701760096904291658135187084551371007366712955491224032388959026156823325454784145354754676577824222200350615079297804422436038119645042066201108523211893066978845349234900329165795636667677676886685475464902400448029...",
+    },
+    {
+      q: "Schreiben Sie in JavaScript eine Funktion topsort(), die eine topologische Sortierung berechnet. Topologische Sortierung ist eine Form von Sortierung. Sie kennen die Funktion Array.prototype.sort() und wissen, was als Ergebnis erwartet wird, nämlich eine sortierte Liste. Genauso ist das hier. Die Funktion topsort() soll eine sortierte Liste ausgeben, die keine der eingegebenen Abhängigkeiten verletzt.",
+      a: `
+function sortTasks(tasks) {
+  let taskGraph = new Map();
+  let incomingEdges = new Map();
+  let taskQueue = [];
+  let sortedOrder = [];
+
+  tasks.forEach(([prerequisite, task]) => {
+    if (!taskGraph.has(prerequisite)) taskGraph.set(prerequisite, []);
+    if (!taskGraph.has(task)) taskGraph.set(task, []);
+
+    taskGraph.get(prerequisite).push(task);
+    incomingEdges.set(task, (incomingEdges.get(task) || 0) + 1);
+    incomingEdges.set(prerequisite, incomingEdges.get(prerequisite) || 0);
+  });
+
+  for (let [task, edgesCount] of incomingEdges) {
+    if (edgesCount === 0) taskQueue.push(task);
+  }
+
+  while (taskQueue.length > 0) {
+    let currentTask = taskQueue.shift();
+    sortedOrder.push(currentTask);
+
+    taskGraph.get(currentTask).forEach(nextTask => {
+      incomingEdges.set(nextTask, incomingEdges.get(nextTask) - 1);
+      if (incomingEdges.get(nextTask) === 0) {
+        taskQueue.push(nextTask);
+      }
+    });
+  }
+
+  return sortedOrder;
+}
+
+console.assert(sortTasks([["schlafen","studieren"],["essen","studieren"],["studieren","prüfen"]]).toString() === ['schlafen', 'essen', 'studieren', 'prüfen'].toString());
+    `,
+    },
+  ]
+)
+kwArr.push(kw45)
+
+const kw46 = parseAsKW("KW 46",
+  [
+    {
+      q: "Implementieren Sie Performanz-Messungen zum Vergleich von innerHTML, innerText, textContent und outerHTML selbstständig in JavaScript durch Nutzung der DOM-API. Geben Sie die Messergebnisse als Tabelle aus. Verwenden Sie die eingebauten Zeitmessung-Funktionen performance.now(), siehe auch When-milliseconds-are-not-enough-performance-now. Suchen Sie eine möglichst kurze und elegante Lösung. Dabei ist zu beachten, dass Browser, um potenzielle Sicherheitsbedrohungen wie Meltdown oder Spectre zu minimieren, den zurückgegebenen Wert normalerweise um einen bestimmten Betrag runden. Dies führt zu einer gewissen Ungenauigkeit. Beispielsweise rundet Firefox die zurückgegebene Zeit in Schritten von 1 Millisekunde. Diese Zwangsrundung kann man jedoch wiederum abschalten mittels der Firefox-Einstellung privacy.reduceTimerPrecision, siehe How to get microsecond timings in JavaScript since Spectre and Meltdown.",
+      a: `
+  <script>
+  const testElement = document.createElement('div');
+  document.body.appendChild(testElement);
+  testElement.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
+  
+  
+  
+  function measurePerformance(method) {
+      const startTime = performance.now();
+      var i = 0;
+      while(i<1000){
+          const result = testElement[method];
+          i++;
+      }
+      const endTime = performance.now();
+      return endTime - startTime;
+  }
+  
+  const results = {
+      'innerHTML': measurePerformance('innerHTML'),
+      'innerText': measurePerformance('innerText'),
+      'textContent': measurePerformance('textContent'),
+      'outerHTML': measurePerformance('outerHTML')
+  };
+  
+  console.log(results);
+  </script>
+    `
+    },
+    {
+      demo: "uebung5.html",
+      q: "Implementieren Sie die interaktive Anwendung \"Rednerliste mit Zeitmessung\" selbstständig in JavaScript durch Nutzung der DOM-API und der Timer-Funktionen. Neue Redner sollen auf Knopfdruck hinzugefügt werden können. Deren Uhr wird dann sofort automatisch gestartet und alle anderen Uhren angehalten. Bei jedem Redner soll die individuelle, gemessene Redezeit sekundengenau angezeigt werden. Für jeden Redner soll es einen eigenen Start-/Stopp-Button geben. Es soll immer nur eine Uhr laufen. Angezeigt werden sollen die bisherigen Summenzeiten aller Redebeiträge der betreffenden Person. Suchen Sie eine möglichst kurze und elegante Lösung. Achten Sie gleichzeitig auf gute Usability: z.B. wenn die Eingabe mit einem Return beendet wird, soll der Klick auf den Button nicht mehr erforderlich sein.",
+      a: `
+  <!DOCTYPE html>
+    <head>
+      <style>
+        #rednerInput {
+          margin-bottom: 10px;
+        }
+        #rednerList {
+          list-style-type: none;
+          padding: 0;
+        }
+        #rednerList li {
+          margin: 5px 0;
+          padding: 5px;
+          background-color: #f0f0f0;
+          border: 1px solid #ddd;
+        }
+      </style>
+    </head>
+    <body>
+      <h2>Rednerliste</h2>
+  
+      <input type="text" id="rednerInput" placeholder="Neuer Redner" />
+      <button id="addButton">Hinzufügen</button>
+  
+      <ul id="rednerList"></ul>
+  
+      <script>
+        document.addEventListener("DOMContentLoaded", (event) => {
+          let timers = {};
+          let activeTimer = null;
+  
+          const addSpeaker = (name) => {
+            if (!name.trim()) return;
+  
+            if (activeTimer) {
+              clearInterval(timers[activeTimer].intervalId);
+              activeTimer = null;
+            }
+  
+            const listItem = document.createElement("li");
+            listItem.textContent = name + " 00:00:00";
+            listItem.id = name;
+            document.getElementById("speakersList").appendChild(listItem);
+  
+            timers[name] = {
+              time: 0,
+              intervalId: setInterval(() => {
+                timers[name].time++;
+                const seconds = timers[name].time % 60;
+                const minutes = Math.floor(timers[name].time / 60) % 60;
+                const hours = Math.floor(timers[name].time / 3600);
+                listItem.textContent = \`${name} \${
+      hours
+                  .toString()
+        .padStart(2, "0")
+    }: \${
+      minutes
+                  .toString()
+        .padStart(2, "0")
+    }: \$\{ seconds.toString().padStart(2, "0") }\`;
+              }, 1000),
+            };
+  
+            activeTimer = name;
+          };
+  
+          document.getElementById("addButton").addEventListener("click", () => {
+            const speakerName = document.getElementById("speakerInput").value;
+            addSpeaker(speakerName);
+            document.getElementById("speakerInput").value = "";
+          });
+  
+          document
+            .getElementById("speakerInput")
+            .addEventListener("keypress", (e) => {
+              if (e.key === "Enter") {
+                const speakerName = e.target.value;
+                addSpeaker(speakerName);
+                e.target.value = "";
+              }
+            });
+        });
+      </script>
+    </body>
+  </html>
+  
+  `
+    },
+    {
+      q: "Schreiben Sie eine Weboberfläche, in der man beliebige Beziehungen (Vorrang-Relationen) eingeben kann, für die dann die topologische Sortierung per Knopfdruck auf der Webseite ausgegeben wird. Für die Eingabe können Sie HTML5-Eingabefelder oder das HTML-Attribut contenteditable verwenden.",
+      a: `
+  <!DOCTYPE html>
+  <html>
+    <body>
+      <h1>Topologische Sortierung</h1>
+      <textarea
+        id="inputRelations"
+        rows="10"
+        cols="50"
+        placeholder='[["x", "y"], ["y", "z"]]'
+      ></textarea
+      ><br />
+      <button onclick="getAndSortTasks()">Sortieren</button>
+      <h2>Sortiert:</h2>
+      <b id="output"></b>
+  
+      <script>
+        function sortTasks(tasks) {
+          let taskGraph = new Map();
+          let incomingEdges = new Map();
+          let taskQueue = [];
+          let sortedOrder = [];
+  
+          tasks.forEach(([prerequisite, task]) => {
+            if (!taskGraph.has(prerequisite)) taskGraph.set(prerequisite, []);
+            if (!taskGraph.has(task)) taskGraph.set(task, []);
+  
+            taskGraph.get(prerequisite).push(task);
+            incomingEdges.set(task, (incomingEdges.get(task) || 0) + 1);
+            incomingEdges.set(prerequisite, incomingEdges.get(prerequisite) || 0);
+          });
+  
+          for (let [task, edgesCount] of incomingEdges) {
+            if (edgesCount === 0) taskQueue.push(task);
+          }
+  
+          while (taskQueue.length > 0) {
+            let currentTask = taskQueue.shift();
+            sortedOrder.push(currentTask);
+  
+            taskGraph.get(currentTask).forEach((nextTask) => {
+              incomingEdges.set(nextTask, incomingEdges.get(nextTask) - 1);
+              if (incomingEdges.get(nextTask) === 0) {
+                taskQueue.push(nextTask);
+              }
+            });
+          }
+  
+          return sortedOrder;
+        }
+        function getAndSortTasks() {
+          const values = JSON.parse(
+            document.querySelector("#inputRelations").value
+          );
+          const sorted = sortTasks(values).join(" -> ");
+          document.querySelector("#output").innerHTML = sorted;
+          console.log(sorted);
+        }
+      </script>
+    </body>
+  </html>
+  `
+    }
+  ]
+);
+kwArr.push(kw46)
+
 
